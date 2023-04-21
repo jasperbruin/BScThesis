222483bd77b1584fcba7e6295c903250c32402e2
jasperbruin <62376435+jasperbruin@users.noreply.github.com>
2023-04-21 15:49:17 +0200

    added RNN model

--- RNNtest.py
+++ RNNtest.py
@@ -1,97 +1,82 @@
-# main script for 'Resource Optimization for Facial Recognition Systems (ROFARS)' project
-# author: Cyril Hsu @ UvA-MNS
-# date: 23/02/2023
-
-from agents import LSTM_RNN_Agent, SlidingWindowUCBAgent
-from rofarsEnv import ROFARS_v1
 import numpy as np
 from tqdm import tqdm
+from rofarsEnv import ROFARS_v1
+from tensorflow.keras.optimizers import Adam
+from agents import baselineAgent, LSTM_Agent
 
 np.random.seed(0)
+
 env = ROFARS_v1()
+best_theta = None
+best_total_reward = -np.inf
 
-n_episode = 1
-max_window_size = 100
-best_window_size = 1
-best_reward = -np.inf
+input_size = env.n_camera
+hidden_size = 16
+output_size = env.n_camera
+lstm_agent = LSTM_Agent(input_size, hidden_size, output_size)
 
+optimizer = Adam(lr=0.001)
+lstm_agent.compile(optimizer, loss='mse')
 
-# Collect necessary features for the LSTM RNN
-train_states = []
-train_rewards = []
-train_actions = []
-
-# Training loop
-for episode in range(n_episode):
-    agent = SlidingWindowUCBAgent(c=3, window_size=31)
-    agent.initialize(env.n_camera)  # Make sure to initialize the agent
+# Training
+for theta in range(5):
     env.reset(mode='train')
-    episode_states = []
-    episode_rewards = []
-    episode_actions = []
+    baseline_agent = baselineAgent(theta=theta)
+    states, actions = [], []
+
+    # Generate training traces from the Baseline agent
+    init_action = np.random.rand(env.n_camera)
+    reward, state, stop = env.step(init_action)
 
     for t in tqdm(range(env.length), initial=2):
-        action = agent.get_action()
+        action = baseline_agent.get_action(state)
+        states.append(state)
+        actions.append(action)
+
         reward, state, stop = env.step(action)
+        if stop:
+            break
 
-        print("action: ", action)
-        print("state: ", state)
-        print("reward: ", reward)
+    # Train the LSTM agent with the training traces
+    states = np.array(states)
+    actions = np.array(actions)
 
-        # Update the UCB Agent
-        agent.update(action, state)
+    states = states.reshape((states.shape[0], 1, states.shape[1]))
+    lstm_agent.fit(states, actions, epochs=10, verbose=1)
 
-        # Save state, reward, and action
-        episode_states.append(state)
-        episode_rewards.append(reward)
-        episode_actions.append(action)
+    # Test the LSTM agent
+    env.reset(mode='test')
+    rewards = []
+
+    for t in tqdm(range(env.length), initial=2):
+        action = lstm_agent.get_action(state)
+        reward, state, stop = env.step(action)
+        rewards.append(reward)
 
         if stop:
             break
 
-    train_states.append(episode_states)
-    train_rewards.append(episode_rewards)
-    train_actions.append(episode_actions)
+    total_reward = np.mean(rewards)
+    if total_reward > best_total_reward:
+        best_theta = theta
+        best_total_reward = total_reward
 
-    total_reward = env.get_total_reward()
-    print(f'=== TRAINING episode {episode} ===')
+    print(f'=== TRAINING theta: {theta} ===')
     print('[total reward]:', total_reward)
 
+print(f'Best found theta: {best_theta}')
 
-# Save the training traces in a dictionary
-training_trace = {
-    'states': train_states,
-    'rewards': train_rewards,
-    'actions': train_actions
-}
-
-# Create the LSTM RNN Agent
-lstm_agent = LSTM_RNN_Agent(record_length=env.n_camera, input_dim=env.n_camera, hidden_dim=32,
-                            learning_rate=0.001, dropout_rate=0.0, l1_reg=0.0,
-                            l2_reg=0.0, epsilon=0.1)
-
-# Train the LSTM RNN Agent using the training_trace
-n_samples = sum([len(episode_states) - 9 for episode_states in train_states])
-
-X = np.concatenate([episode_states[i:i+10] for episode_states in train_states for i in range(len(episode_states) - 9)]).reshape(n_samples, 10, env.n_camera)
-y = np.concatenate([episode_actions[i+9] for episode_actions in train_actions for i in range(len(episode_actions) - 9)]).reshape(n_samples, env.n_camera)
-
-lstm_agent.train_on_batch(X, y)
-
-
-# Testing loop
+# Testing
 env.reset(mode='test')
-test_rewards = []
+rewards = []
 
 for t in tqdm(range(env.length), initial=2):
     action = lstm_agent.get_action(state)
     reward, state, stop = env.step(action)
-
-    test_rewards.append(reward)
+    rewards.append(reward)
 
     if stop:
         break
 
-test_total_reward = np.sum(test_rewards)
-print(f'====== TESTING LSTM RNN Agent ======')
-print('[total reward]:', test_total_reward)
+print(f'====== TESTING ======')
+print('[total reward]:', np.mean(rewards))
--- experiments/agents.py
+++ experiments/agents.py
@@ -7,9 +7,9 @@ from collections import deque
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import SimpleRNN, Dense, Masking
-from tensorflow.keras.layers import LSTM, Dropout, Dense
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.regularizers import L1L2
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import LSTM, Dense, Input
 import numpy as np
 
 
@@ -49,84 +49,6 @@ class baselineAgent:
     def clear_records(self):
         self.records = None
 
-
-class LSTM_RNN_Agent:
-    def __init__(self, record_length=10, input_dim=1, hidden_dim=32,
-                 learning_rate=0.001, n_lstm_layers=1, dropout_rate=0.0,
-                 l1_reg=0.0, l2_reg=0.0, epsilon=0.1):
-        self.records = [[] for _ in range(input_dim)]  # Initialize as an empty list of lists
-        self.record_length = record_length
-        self.moving_avg = 0
-        self.epsilon = epsilon
-        self.model = self.build_model(input_dim, hidden_dim, learning_rate,
-                                      n_lstm_layers, dropout_rate, l1_reg,
-                                      l2_reg)
-
-    def build_model(self, input_dim, hidden_dim, learning_rate, n_lstm_layers,
-                    dropout_rate, l1_reg, l2_reg):
-        with tf.device('/gpu:0'):
-            model = Sequential()
-            for i in range(n_lstm_layers):
-                if i == 0:
-                    model.add(LSTM(hidden_dim,
-                                   input_shape=(self.record_length, input_dim),
-                                   return_sequences=True if n_lstm_layers > 1 else False))
-                else:
-                    model.add(LSTM(hidden_dim,
-                                   return_sequences=True if i < n_lstm_layers - 1 else False))
-                if dropout_rate > 0:
-                    model.add(Dropout(dropout_rate))
-
-            model.add(Dense(input_dim, activation='softmax',
-                            kernel_regularizer=L1L2(l1=l1_reg, l2=l2_reg)))
-            model.compile(loss='categorical_crossentropy',
-                          optimizer=Adam(learning_rate=learning_rate))
-            return model
-
-    def get_action(self, state):
-        self.add_record(state)
-        imputed_state = self.impute_missing_values(state)
-        input_data = np.array(self.records).T.reshape(-1, self.record_length, 1)
-
-        # Exploration vs. Exploitation
-        if np.random.rand() < self.epsilon:
-            action = np.random.rand(len(state))
-        else:
-            action = self.model.predict(input_data)
-
-        action = action.flatten() / action.sum()
-        return action
-
-    def add_record(self, state):
-        if not self.records or not self.records[0]:  # Check for empty list of lists
-            self.records = [[] for _ in range(len(state))]
-        for r, s in zip(self.records, state):
-            r.append(s)
-            if len(r) > self.record_length:
-                r.pop(0)
-
-    def clear_records(self):
-        self.records = [[] for _ in range(len(self.records))]  # Clear records while maintaining the structure
-
-    def train_on_batch(self, X, y):
-        with tf.device('/gpu:0'):
-            loss, accuracy = self.model.train_on_batch(X, y), None
-            metrics_values = self.model.evaluate(X, y, verbose=0)
-            if isinstance(metrics_values, float):
-                # If metrics_values is a float, set accuracy directly
-                accuracy = metrics_values
-            else:
-                # Otherwise, calculate accuracy from metrics_values
-                accuracy = metrics_values[1]
-            return loss, accuracy
-
-    def impute_missing_values(self, state):
-        """Impute missing values in the state with the mean of historical records."""
-        imputed_state = np.copy(state)
-        for i, s in enumerate(state):
-            if s == -1:
-                imputed_state[i] = np.mean(self.records[i])
-        return imputed_state
 
 class UCBAgent:
     def __init__(self, c=2):
@@ -253,3 +175,38 @@ class DQNAgent:
         else:
             act_values = self.model.predict(state)
             return act_values[0]
+
+
+class LSTM_Agent(Model):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM_Agent, self).__init__()
+        self.lstm = LSTM(hidden_size, return_sequences=False)
+        self.dense = Dense(output_size, activation='softmax')
+
+        self.input_layer = Input(shape=(1, input_size))
+        self.output_layer = self.dense(self.lstm(self.input_layer))
+        self.model = Model(self.input_layer, self.output_layer)
+
+        self.records = [[] for _ in range(input_size)]
+
+    def call(self, inputs):
+        x = self.lstm(inputs)
+        x = self.dense(x)
+        return x
+
+    def get_action(self, state):
+        imputed_state = self.impute_missing_values(state)
+        state = np.expand_dims(imputed_state, axis=0)
+        state = np.expand_dims(state, axis=1)
+        action = self.model.predict(state)
+        return action[0]
+
+    def impute_missing_values(self, state):
+        """Impute missing values in the state with the mean of historical records."""
+        imputed_state = np.copy(state)
+        for i, s in enumerate(state):
+            if s == -1:
+                imputed_state[i] = np.mean(self.records[i]) if len(self.records[i]) > 0 else 0
+            else:
+                self.records[i].append(s)
+        return imputed_state
