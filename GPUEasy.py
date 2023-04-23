3337849c17028927b48084f55b670922ffad03bc
jasperbruin <62376435+jasperbruin@users.noreply.github.com>
2023-04-23 13:13:45 +0200

    Update GPUEasy.py

--- GPUEasy.py
+++ GPUEasy.py
@@ -1,58 +1,14 @@
-import numpy as np
-from tqdm import tqdm
 from rofarsEnv import ROFARS_v1
+from scipy.interpolate import interp1d
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from scipy.interpolate import interp1d
-from scipy.interpolate import UnivariateSpline
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+from tqdm import tqdm
 
 device = torch.device("mps")
-
-class DiscountedUCBAgent:
-    def __init__(self, gamma=0.9):
-        self.counts = None
-        self.discounted_counts = None
-        self.values = None
-        self.discounted_rewards = None
-        self.c = 3
-        self.gamma = gamma
-        self.total_time_steps = 0
-
-    def initialize(self, n_actions):
-        self.counts = np.zeros(n_actions)
-        self.discounted_counts = np.zeros(n_actions)
-        self.values = np.zeros(n_actions)
-        self.discounted_rewards = np.zeros(n_actions)
-
-    def get_action(self):
-        if self.counts.min() == 0:
-            idx = np.random.choice(np.where(self.counts == 0)[0])
-            action = np.zeros(len(self.values))
-            action[idx] = 1
-        else:
-            discounted_means = self.discounted_rewards / self.discounted_counts
-            ct_numerator = 2 * np.log(self.total_time_steps)
-            ct_denominator = self.discounted_counts
-            ct = self.c * np.sqrt(np.maximum(ct_numerator / ct_denominator, 0))
-            action = discounted_means + ct
-        return action
-
-    def update(self, actions, state):
-        self.total_time_steps = self.gamma*self.total_time_steps + 1
-        self.discounted_counts *= self.gamma
-        self.discounted_rewards *= self.gamma
-
-        for i, reward in enumerate(state):
-            if reward >= 0:
-                self.counts[i] += 1
-                self.values[i] += reward
-
-                self.discounted_counts[i] += 1
-                self.discounted_rewards[i] += reward
-            else:
-                self.counts[i] += 0
-
 
 class baselineAgent:
     def __init__(self, theta=0, agent_type='strong'):
@@ -121,106 +77,55 @@ class LSTM_Agent(nn.Module):
 
 
 
-def evaluateUCB():
-    lr = float(input('Learning rate: '))
-    epochs = int(input('Epochs: '))
-    hidden_size = int(input('Hidden size: '))
-
-    env = ROFARS_v1()
-    best_total_reward = -np.inf
-    input_size = env.n_camera
-    output_size = env.n_camera
-    lstm_agent = LSTM_Agent(input_size, hidden_size, output_size).to(device)
-    optimizer = optim.Adam(lstm_agent.parameters(), lr=lr)
-    criterion = nn.MSELoss()
-
-    # Training
-    baseline_agent = DiscountedUCBAgent(gamma=0.999)
-    baseline_agent.initialize(env.n_camera)
-    env.reset(mode='train')
-
-    states, actions = [], []
-
-    # Generate training traces from the Baseline agent
-    init_action = np.random.rand(env.n_camera)
-    reward, state, stop = env.step(init_action)
-
-    for t in tqdm(range(env.length), initial=2):
-        action = baseline_agent.get_action()
-        reward, state, stop = env.step(action)
-        baseline_agent.update(action, state)
-
-
-        states.append(state)
-        actions.append(action)
-
-        if stop:
-            break
-
-    # Train the LSTM agent with the training traces
-    states = np.array(states)
-    actions = np.array(actions)
-
-    states = states.reshape((states.shape[0], 1, states.shape[1]))
-    states_tensor = torch.tensor(states, dtype=torch.float32).to(device)
-    actions_tensor = torch.tensor(actions, dtype=torch.float32).to(device)
-
+def train_lstm_agent(agent, dataloader, criterion, optimizer, epochs):
+    agent.train()
     for epoch in range(epochs):
-        optimizer.zero_grad()
-        predictions = lstm_agent(states_tensor)
-        loss = criterion(predictions, actions_tensor)
-        loss.backward()
-        optimizer.step()
-        print(f'Epoch {epoch + 1}, Loss: {loss.item():.3f}')
+        running_loss = 0.0
+        for states, actions in dataloader:
+            optimizer.zero_grad()
+            predictions = agent(states)
+            loss = criterion(predictions, actions)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
 
-    # Test the LSTM agent
+        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(dataloader):.3f}')
+
+
+def test_lstm_agent(agent, env):
+    agent.eval()
     env.reset(mode='test')
     rewards = []
 
+    state = env.reset()
     for t in tqdm(range(env.length), initial=2):
-        action = lstm_agent.get_action(state)
+        action = agent.get_action(state)
         reward, state, stop = env.step(action)
         rewards.append(reward)
 
         if stop:
             break
 
-    total_reward = np.mean(rewards)
-    if total_reward > best_total_reward:
-        best_total_reward = total_reward
-
-    print(f'=== TRAINING ===')
-    print('[total reward]:', f'{total_reward:.3f}')
-
-    # Testing
-    env.reset(mode='test')
-    rewards = []
-
-    for t in tqdm(range(env.length), initial=2):
-        action = lstm_agent.get_action(state)
-        reward, state, stop = env.step(action)
-        rewards.append(reward)
-
-        if stop:
-            break
-
-    print(f'====== TESTING ======')
-    print('[total reward]:', f'{np.mean(rewards):.3f}')
+    return np.mean(rewards)
 
 
 def evaluateBaseline():
     lr = float(input('Learning rate: '))
     epochs = int(input('Epochs: '))
     hidden_size = int(input('Hidden size: '))
+    batch_size = int(input('Batch size: '))
+    loss_function = int(input('Loss function (1: MSE, 2: MAE): '))
 
     env = ROFARS_v1()
-    best_total_reward = -np.inf
     input_size = env.n_camera
     output_size = env.n_camera
     lstm_agent = LSTM_Agent(input_size, hidden_size, output_size).to(device)
     optimizer = optim.Adam(lstm_agent.parameters(), lr=lr)
-    criterion = nn.MSELoss()
 
+    if loss_function == 1:
+        criterion = nn.MSELoss()
+    else:
+        criterion = nn.L1Loss()
 
     # Training
     env.reset(mode='train')
@@ -248,54 +153,18 @@ def evaluateBaseline():
     states_tensor = torch.tensor(states, dtype=torch.float32).to(device)
     actions_tensor = torch.tensor(actions, dtype=torch.float32).to(device)
 
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        predictions = lstm_agent(states_tensor)
-        loss = criterion(predictions, actions_tensor)
-        loss.backward()
-        optimizer.step()
-        print(f'Epoch {epoch + 1}, Loss: {loss.item():.3f}')
+    train_dataset = TensorDataset(states_tensor, actions_tensor)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    train_lstm_agent(lstm_agent, train_dataloader, criterion, optimizer, epochs)
 
     # Test the LSTM agent
-    env.reset(mode='test')
-    rewards = []
-
-    for t in tqdm(range(env.length), initial=2):
-        action = lstm_agent.get_action(state)
-        reward, state, stop = env.step(action)
-        rewards.append(reward)
-
-        if stop:
-            break
-
-    total_reward = np.mean(rewards)
-    if total_reward > best_total_reward:
-        best_total_reward = total_reward
-
-    print(f'=== TRAINING ===')
-    print('[total reward]:', f'{total_reward:.3f}')
-
-    # Testing
-    env.reset(mode='test')
-    rewards = []
-
-    for t in tqdm(range(env.length), initial=2):
-        action = lstm_agent.get_action(state)
-        reward, state, stop = env.step(action)
-        rewards.append(reward)
-
-        if stop:
-            break
-
+    test_reward = test_lstm_agent(lstm_agent, env)
     print(f'====== TESTING ======')
-    print('[total reward]:', f'{np.mean(rewards):.3f}')
+    print('[total reward]:', f'{test_reward:.3f}')
 
 if __name__ == '__main__':
-    inp = int(input('1. Baseline\n2. UCB\n'))
-    if inp == 1:
-        evaluateBaseline()
-    elif inp == 2:
-        evaluateUCB()
+    evaluateBaseline()
 
 
 """
