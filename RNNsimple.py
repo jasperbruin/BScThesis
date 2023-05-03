e045e0eaf1067db61f402b4442153df0ea3d5ee1
jasperbruin <62376435+jasperbruin@users.noreply.github.com>
2023-05-03 09:48:43 +0200

    Update RNNsimple.py

--- RNNsimple.py
+++ RNNsimple.py
@@ -201,13 +201,11 @@ if __name__ == '__main__':
 
         # Add the current state to the last_states deque
         last_states.append(state)
-
-
-
-
         # Prepare the input state for the LSTM agent
         input_state = np.vstack(list(last_states) + [state])  # Combine the last_states with the current state
         input_state = torch.tensor(input_state, dtype=torch.float32).unsqueeze(0)  # Add the batch dimension
+
+        print(input_state)
 
         # Get the action from the LSTM agent
         action = lstm_agent(input_state).squeeze().detach().numpy()
