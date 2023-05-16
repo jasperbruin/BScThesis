addf613a92d7b28ce0256f60ce43cea4ce45c29d
jasperbruin <62376435+jasperbruin@users.noreply.github.com>
2023-05-16 09:17:51 +0200

    yes

--- RNNnobatch.py
+++ RNNnobatch.py
@@ -27,7 +27,7 @@ agent = None
 l_rate = 0.001
 hidden_size = 16
 time_steps = [60]
-epochs = 10000
+epochs = 5000
 patience = 5
 
 best_val_loss = float('inf')
@@ -35,7 +35,7 @@ epochs_without_improvement = 0
 result = []
 
 
-# ['Agent', 'Total Reward', 'Epochs', 'Learning Rate', 'Batch Size', 'Hidden Size', 'Time Steps', 'Loss Function']
+# ['Agent', 'Total Reward', 'Epochs', 'Learning Rate', 'Hidden Size', 'Time Steps', 'Loss Function']
 
 
 
@@ -247,121 +247,3 @@ if __name__ == '__main__':
             writer = csv.writer(file)
             for row in result:
                 writer.writerow(row)
-
-
-"""
-====== RECORD ======
-SW-UCB Agent: 0.562
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.501
-[Hyperparameters]:  1000 0.001 64 60 64 5 1
-
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.516
-[Hyperparameters]: 
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 120 loss function: 1
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.51
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 128 
-hidden_size: 32 time_steps: 120 loss function: 1
-
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.501
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 180 loss function: 1
-
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.517
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 240 loss function: 1
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.496
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 300 loss function: 1
-
-
-"""
-
-
-"""
-====== RESULT ======
-D-UCB Agent
-[total reward]: 0.504
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 120 loss function: 1
-
-====== RESULT ======
-SW-UCB Agent
-[total reward]: 0.493
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 32 
-hidden_size: 32 time_steps: 120 loss function: 1
-"""
-
-"""
-====== TESTING======
-[total reward]: 0.559
-Best gamma: 0.999
-
-Difference Strong Baseline = Run 3 - Baseline = 0.559 - 0.506 = 0.053
-Percentage growth = (Difference / Baseline) x 100 = 0.053 / 0.506 x 100 = 10.5%
-
-Difference Weak Baseline = Run 3 - Baseline = 0.559 - 0.317 = 0.242
-Percentage growth = (Difference / Baseline) x 100 = 0.242 / 0.317 x 100 = 76.2%
-
-
-====== RESULT ======
-Baseline Agent
-[total reward]: 0.517
-
-Difference Strong Baseline = Run 3 - Baseline = 0.517 - 0.506 = 0.011
-Percentage growth = (Difference / Baseline) x 100 = 0.011 / 0.506 x 100 = 2.2%
-
-Difference Weak Baseline = Run 3 - Baseline = 0.517 - 0.317 = 0.2
-Percentage growth = (Difference / Baseline) x 100 = 0.2 / 0.317 x 100 = 63.1%
-
-
-====== RESULT ======
-D-UCB Agent
-[total reward]: 0.498
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 240 loss function: 1
-
-Difference Strong Baseline = Run 3 - Baseline = 0.498 - 0.506 = -0.008
-Percentage growth = (Difference / Baseline) x 100 = -0.008 / 0.506 x 100 = -1.6%
-
-Difference Weak Baseline = Run 3 - Baseline = 0.498 - 0.317 = 0.181
-Percentage growth = (Difference / Baseline) x 100 = 0.181 / 0.317 x 100 = 57.1%
-
-
-
-====== RESULT ======
-SW-UCB Agent
-[total reward]: 0.499
-[Hyperparameters]
-epochs: 1000 lr: 0.001 batch_size: 64 
-hidden_size: 32 time_steps: 240 loss function: 1
-
-Difference Strong Baseline = Run 3 - Baseline = 0.499 - 0.506 = -0.007
-
-
-"""
